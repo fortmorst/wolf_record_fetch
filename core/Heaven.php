be698408605b9036b9218f8cd73bc0e52a5993a1
fortmorst <fortmorst@server.fake>
2015-06-29 23:08:07 +0900

    pre-complete

--- core/Heaven.php
+++ core/Heaven.php
@@ -18,8 +18,8 @@ class Heaven extends Country
 
     $this->fetch_name();
     $this->fetch_days();
-    $this->fetch_rglid();
     $this->fetch_type();
+    $this->fetch_policy();
 
     $this->fetch->clear();
   }
@@ -28,60 +28,45 @@ class Heaven extends Country
     $name = $this->fetch->find('h2',0)->plaintext;
     $this->village->name = mb_ereg_replace('\d+村 (.+)','\1',$name);
   }
-  protected function fetch_type()
-  {
-    $type = $this->fetch->find('table.vil_main table',1)->find('td',5)->plaintext;
-    if($type === 'カード人狼形式')
-    {
-      $this->village->is_card = true;
-      echo $this->village->vno.' is card'.PHP_EOL;
-      $this->village->days = $this->village->days +2;
-    }
-    else
-    {
-      $this->village->is_card = false;
-    }
-  }
-  protected function fetch_nop()
-  {
-  }
-  protected function fetch_rglid()
-  {
-    $rgl = $this->fetch->find('table',3)->find('td',5)->plaintext;
-    if($rgl === '？')
-    {
-      $this->village->nop = 16;
-    }
-    else
-    {
-      $this->village->nop = mb_strlen($rgl);
-    }
-
-    if(array_key_exists($rgl,$this->rgl))
-    {
-      $this->village->rglid = $this->rgl[$rgl];
-    }
-    else
-    {
-      $this->village->rglid = Data::RGL_ETC;
-    }
-  }
   protected function fetch_days()
   {
-    $days = trim($this->fetch->find('a',-4)->plaintext);
-    if($days === '全')
-    {
-      $days = trim($this->fetch->find('table.vil_main a',-7)->plaintext);
-    }
+    $days = $this->fetch->find('a',-3)->plaintext;
     $this->village->days = mb_substr($days,0,mb_strpos($days,'日')) -1;
     if($this->village->days < 2)
     {
       echo 'NOTICE: '.$this->village->vno.' has just '.$this->village->days.' days.'.PHP_EOL;
     }
   }
+  protected function fetch_type()
+  {
+    $type = $this->fetch->find('dl.explain span',0)->plaintext;
+    if($type === 'カード人狼形式 ')
+    {
+      $this->village->is_card = true;
+      echo $this->village->vno.' is card'.PHP_EOL;
+      $this->village->days = $this->village->days +1;
+    }
+    else
+    {
+      $this->village->is_card = false;
+    }
+  }
+  protected function fetch_policy()
+  {
+    $policy = $this->fetch->find('table table table tr td',13)->plaintext;
+    if(mb_strpos($policy,"ガチ推理") !== false)
+    {
+      $this->village->policy = true;
+    }
+    else
+    {
+      $this->village->policy = false;
+      $this->output_comment('rp');
+    }
+  }
   protected function fetch_from_pro()
   {
-    $url = $this->url.$this->village->vno.'_1.html';
+    $url = $this->url.$this->village->vno.'&date=1&start=1';
     $this->fetch->load_file($url);
 
     $this->fetch_date();
@@ -89,21 +74,17 @@ class Heaven extends Country
   }
   protected function fetch_date()
   {
-    $date = $this->fetch->find('span.time',1)->plaintext;
+    $date = $this->fetch->find('span.time',0)->plaintext;
     $date = mb_substr($date,0,10);
     $this->village->date = preg_replace('/(\d{4})\/(\d{2})\/(\d{2})/','\1-\2-\3',$date);
   }
   protected function fetch_from_epi()
   {
-    if($this->village->is_card)
-    {
-      $day = $this->village->days-1;
-    }
-    else
+    if(!$this->village->is_card)
     {
       $day = $this->village->days+1;
     }
-    $url = $this->url.$this->village->vno.'_'.$day.'.html';
+    $url = $this->url.$this->village->vno.'&start=1&date='.$day;
     $this->fetch->load_file($url);
 
     $this->fetch_wtmid();
@@ -111,24 +92,31 @@ class Heaven extends Country
   }
   protected function fetch_wtmid()
   {
-    $wtmid = mb_substr(trim($this->fetch->find('div.win',0)->plaintext),0,2);
-    $this->village->wtmid = $this->team[$wtmid];
+    $wtmid = mb_substr($this->fetch->find('div.win',0)->plaintext,0,2);
+    $this->village->wtmid = $this->TEAM[$wtmid];
   }
   protected function make_cast()
   {
-    $cast = $this->fetch->find('table.member tr td');
+    $cast = $this->fetch->find('table.list tr');
+    array_shift($cast);
     foreach($cast as $key=>$item)
     {
-      $line = trim($item->plaintext);
-      switch(mb_substr($line,0,2))
+      $line = $item->plaintext;
+      if(mb_ereg_match('^  ',$line))
       {
-        case '':
-        case '☆生':
-        case '☆突':
-        case '☆処':
+        unset($cast[$key]);
+        continue;
+      }
+
+      $line = trim($line);
+      switch(mb_substr($line,0,5))
+      {
+        case '[─] 処':
+        case '[─] 観':
+        case 'ハッシュタ':
           unset($cast[$key]);
           break;
-        case '☆犠':
+        case '[─] 犠':
           $this->key_alive = $key;
           unset($cast[$key]);
           break;
@@ -172,13 +160,44 @@ class Heaven extends Country
 
     $this->user->persona = $person[0];
     $this->user->player = mb_substr($person[1],4);
-    $this->user->role = $person[2];
+    $this->user->role = $person[3];
 
     $this->fetch_sklid();
     $this->fetch_rltid();
     if($this->is_alive($key))
     {
       $this->insert_alive();
+    }
+  }
+  protected function fetch_sklid()
+  {
+    $is_love = mb_strpos($this->user->role,'(');
+    if($is_love === false)
+    {
+      $this->user->sklid = $this->skill[$this->user->role][0];
+      $this->user->tmid = $this->skill[$this->user->role][1];
+    }
+    else
+    {
+      $role = mb_substr($this->user->role,0,$is_love-1);
+      $this->user->sklid = $this->skill[$role][0];
+      $this->user->tmid = Data::TM_LOVERS;
+    }
+  }
+  protected function fetch_rltid()
+  {
+    if($this->village->wtmid === 0)
+    {
+      $this->user->rltid = Data::RSL_JOIN;
+      return;
+    }
+    if($this->user->tmid === $this->village->wtmid)
+    {
+      $this->user->rltid = Data::RSL_WIN;
+    }
+    else
+    {
+      $this->user->rltid = Data::RSL_LOSE;
     }
   }
   protected function is_alive($key)
@@ -198,37 +217,11 @@ class Heaven extends Country
     $this->user->end = $this->village->days;
     $this->user->life = 1.000;
   }
-  protected function fetch_sklid()
-  {
-    $is_love = mb_strpos($this->user->role,'（');
-    if($is_love === false)
-    {
-      $this->user->sklid = $this->skill[$this->user->role][0];
-      $this->user->tmid = $this->skill[$this->user->role][1];
-    }
-    else
-    {
-      $role = mb_substr($this->user->role,0,$is_love);
-      $this->user->sklid = $this->skill[$role][0];
-      $this->user->tmid = Data::TM_LOVERS;
-    }
-  }
-  protected function fetch_rltid()
-  {
-    if($this->user->tmid === $this->village->wtmid)
-    {
-      $this->user->rltid = Data::RSL_WIN;
-    }
-    else
-    {
-      $this->user->rltid = Data::RSL_LOSE;
-    }
-  }
   protected function fetch_from_daily($list)
   {
     if($this->village->is_card)
     {
-      $days = $this->village->days-1;
+      //$days = $this->village->days-1;
       $start = 2;
     }
     else
@@ -239,10 +232,10 @@ class Heaven extends Country
 
     for($i=$start; $i<=$days; $i++)
     {
-      $url = $this->url.$this->village->vno.'_'.$i.'.html';
+      $url = $this->url.$this->village->vno.'&start=1&date='.$i;
       $this->fetch->load_file($url);
-      $system = $this->check_destiny($i,'system',$list);
-      $kill = $this->check_destiny($i,'kill',$list);
+      $this->check_destiny($i,'vote',$list); //突然死もvote
+      $this->check_destiny($i,'whisper',$list);
       $this->fetch->clear();
     }
   }
--- country/Raaru.php
+++ country/Raaru.php
@@ -7,6 +7,7 @@ class Raaru extends Heaven
      ,"背徳者"=>[Data::SKL_FRY_FANATIC,Data::TM_FAIRY]
      ,"契約者"=>[Data::SKL_QP,Data::TM_LOVERS]
      ,"共犯者"=>[Data::SKL_QP_SELF,Data::TM_LOVERS]
+     ,"観戦者"=>[Data::SKL_ONLOOKER,Data::TM_ONLOOKER]
   ];
   function __construct()
   {
