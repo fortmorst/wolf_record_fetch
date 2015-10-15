3fa5b36649aa98ca5968ba10d1903c0d9bcfd98c
fortmorst <fortmorst@server.fake>
2015-10-15 12:44:21 +0900

    fix: 深海国一時移転

--- country/Sea_Red.php
+++ country/Sea_Red.php
@@ -5,8 +5,8 @@ class Sea_Red extends Sea
   function set_village_data()
   {
     $cid = 45;
-    $url_vil = "http://redabyss.sixcore.jp/abyss/sow.cgi?vid=";
-    $url_log = "http://redabyss.sixcore.jp/abyss/sow.cgi?cmd=oldlog";
+    $url_vil = "http://chaos-circle.sakura.ne.jp/escape/sow.cgi?vid=";
+    $url_log = "http://chaos-circle.sakura.ne.jp/escape/sow.cgi?cmd=oldlog";
     return ['cid'=>$cid,'url_vil'=>$url_vil,'url_log'=>$url_log];
   }
 }
