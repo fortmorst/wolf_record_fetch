1e786ba0d09b303471a3bb68636a6678af3d4418
fortmorst <fortmorst@server.fake>
2015-12-14 07:55:26 +0900

    fix: URL変更

--- country/Sea_Red.php
+++ country/Sea_Red.php
@@ -5,8 +5,8 @@ class Sea_Red extends Sea
   function set_village_data()
   {
     $cid = 45;
-    $url_vil = "http://chaos-circle.sakura.ne.jp/escape/sow.cgi?vid=";
-    $url_log = "http://chaos-circle.sakura.ne.jp/escape/sow.cgi?cmd=oldlog";
+    $url_vil = "http://red-abyss.net/abyss/sow.cgi?vid=";
+    $url_log = "http://red-abyss.net/abyss/sow.cgi?cmd=oldlog";
     return ['cid'=>$cid,'url_vil'=>$url_vil,'url_log'=>$url_log];
   }
 }
